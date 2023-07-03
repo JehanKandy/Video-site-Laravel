7dabac0fa6d758ab1865d08c3fdfce57b3278ffb
JehanKandy <jehankandy@gmail.com>
2023-07-03 20:53:12 +0530

    Update

--- database/migrations/2023_07_03_120204_create_videos_table.php
+++ database/migrations/2023_07_03_120204_create_videos_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('video_title');
             $table->string('uploader');
             $table->longText('description');
-            $table->json('video_tags');
             $table->timestamps();
         });
     }
