ac35debda73029a056419381ce136c9e0ace19c4
JehanKandy <jehankandy@gmail.com>
2023-07-03 21:54:34 +0530

    Update

--- app/Models/VideoM.php
+++ app/Models/VideoM.php
@@ -4,9 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\VideoTags;
 
 class VideoM extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'id',
+        'video_title',
+        'uploader',
+        'description',
+        'created_at',
+        'updated_at',
+    ];
+  
+    public function VideoTags()
+    {
+        return $this->belongsTo(VideoTags::class);
+    }
 
 }
