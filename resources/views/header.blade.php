eca3e49dab2c1ef9f32f09f334d73ecfd40c3b87
JehanKandy <jehankandy@gmail.com>
2023-07-03 17:25:37 +0530

    Update

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,7 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>User Management</title>
+    <title>Video Site</title>
+    <link rel="icon" href="">
+
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
