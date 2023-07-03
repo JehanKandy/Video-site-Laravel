015a32c062f5747c10af01f47ded1d0b56f94231
JehanKandy <jehankandy@gmail.com>
2023-07-04 00:12:56 +0530

    Update

--- resources/views/videos/create.blade.php
+++ resources/views/videos/create.blade.php
@@ -6,5 +6,11 @@
         <a href="{{ url('/videos') }}"><button class="btn btn-primary">Back</button></a><br><br>
         <hr>
         <h2>Add New Video</h2>
+
+        <form action="{{ url('/videos') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+
+            
+        </form>
     </div>
 @endsection
