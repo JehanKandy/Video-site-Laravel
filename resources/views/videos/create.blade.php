a38387bdfe265a99f39b6e9cc72d475c0e546ef6
JehanKandy <jehankandy@gmail.com>
2023-07-04 00:23:09 +0530

    Update

--- resources/views/videos/create.blade.php
+++ resources/views/videos/create.blade.php
@@ -5,7 +5,7 @@
     <div class="admin-card">
         <a href="{{ url('/videos') }}"><button class="btn btn-primary">Back</button></a><br><br>
         <hr>
-        <h2>Add New Video</h2>
+        <h2>Add New Video</h2><br>
 
         <form action="{{ url('/videos') }}" method="POST" enctype="multipart/form-data">
             @csrf
@@ -16,7 +16,10 @@
                     <input type="text" id="" class="form-control" value="@php echo Auth::user()->name; @endphp" disabled>
                     <input type="hidden" name="uploader" value="@php echo Auth::user()->name; @endphp">
                 </div> 
-                <div class="col-lg-6"></div>
+                <div class="col-lg-6">
+                    <label for="video_title">Video Title</label>
+                    
+                </div>
             </div>
 
         </form>
