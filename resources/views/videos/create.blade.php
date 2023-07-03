f2c207167520cd9b659544a3efe67258c66ab587
JehanKandy <jehankandy@gmail.com>
2023-07-04 00:27:09 +0530

    Update

--- resources/views/videos/create.blade.php
+++ resources/views/videos/create.blade.php
@@ -27,6 +27,16 @@
                     <textarea name="description" id="" cols="30" rows="10" class="form-control" style="resize: none;"></textarea>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-lg-6">
+                    <label for="tags">Tags</label>
+                    <input type="text" name="tags" id="" class="form-control"><br>
+                </div>
+                <div class="col-lg-6">
+                    <label for="video">Video</label>
+                    <input type="file" name="video" id="" class="form-control"><br>
+                </div>
+            </div>
 
         </form>
     </div>
