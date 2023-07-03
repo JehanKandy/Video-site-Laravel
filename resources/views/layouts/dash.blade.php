b06c9e99be124b7d0565c23f4344b85a6decf4eb
JehanKandy <jehankandy@gmail.com>
2023-07-03 17:21:44 +0530

    Update

--- resources/views/layouts/dash.blade.php
+++ resources/views/layouts/dash.blade.php
@@ -139,7 +139,14 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-transparent py-4 px-4">
             <div class="d-flex align-items-center">
                 <i class="fas fa-align-left primary-text fs-4 me-3" id="menu-toggle"></i>
-                <h2 class="fs-2 m-0">Dashboard</h2>
+                <h2 class="fs-2 m-0">
+                    @php
+                        if(auth()->user()->role == 1){
+                            echo "Admin Dashboard";
+                        }
+                    @endphp
+
+                </h2>
             </div>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
