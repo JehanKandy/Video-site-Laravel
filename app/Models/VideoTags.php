84f21681deb1005e7d6a248040d59e0806017d01
JehanKandy <jehankandy@gmail.com>
2023-07-03 21:52:09 +0530

    Update

--- app/Models/VideoTags.php
+++ app/Models/VideoTags.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\VideoM;
 
 class VideoTags extends Model
 {
     use HasFactory;
-    
+    protected $fillable = [
+        'id',
+        'v_id',
+        'tags',
+    ];
+  
+    public function products()
+    {
+        return $this->hasMany(VideoM::class, 'v_id', 'id');
+    }
+
 }
