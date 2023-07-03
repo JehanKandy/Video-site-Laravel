44cbd7d0a70746ad6690ae2e97e0965ac660b662
JehanKandy <jehankandy@gmail.com>
2023-07-03 21:15:12 +0530

    Update

--- app/Models/VideoM.php
+++ app/Models/VideoM.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class VideoM extends Model
 {
     use HasFactory;
+
 }
--- app/Models/VideoTags.php
+++ app/Models/VideoTags.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class VideoTags extends Model
 {
     use HasFactory;
+    
 }
