2da351845913f7519657df5901721e0443b748f5
JehanKandy <jehankandy@gmail.com>
2023-07-04 00:04:43 +0530

    Update

--- resources/views/videos/create.blade.php
+++ resources/views/videos/create.blade.php
@@ -1,1 +1,9 @@
 @extends('layouts.dash')
+
+
+@section('content')
+    <div class="admin-card">
+        <a href="{{ url('/videos') }}"><button class="btn btn-primary">Back</button></a><br><br>
+        <hr>
+    </div>
+@endsection
