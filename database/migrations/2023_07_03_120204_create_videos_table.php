6ef37992eeae2fcc4edcbd593d373d7ae6d645ea
JehanKandy <jehankandy@gmail.com>
2023-07-03 17:38:06 +0530

    Update

--- database/migrations/2023_07_03_120204_create_videos_table.php
+++ database/migrations/2023_07_03_120204_create_videos_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('videos', function (Blueprint $table) {
             $table->id();
+            $table->string('video_title');
+            $table->string('uploader');
+            $table->longText('description');
             $table->timestamps();
         });
     }
