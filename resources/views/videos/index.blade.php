93c0d73652aec5e769909c597ec7610e45f39dd6
JehanKandy <jehankandy@gmail.com>
2023-07-03 22:21:13 +0530

    Update

--- resources/views/videos/index.blade.php
+++ resources/views/videos/index.blade.php
@@ -2,6 +2,21 @@
 
 @section('content')
     <div class="admin-card">
-        
+        <table class="table">
+            <thead class="table-primary">
+                <tr>
+                    <th>ID</th>
+                    <th>Video Uploader</th>
+                    <th>Video Description</th>
+                    <th>VideoTitle</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($collection as $item)
+                    
+                @endforeach
+            </tbody>
+        </table>
     </div>
 @endsection
