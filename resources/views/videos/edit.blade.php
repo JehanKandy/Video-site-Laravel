b49380e1224259263c51cadc24446296452dcee7
JehanKandy <jehankandy@gmail.com>
2023-07-04 10:44:16 +0530

    Update

--- resources/views/videos/edit.blade.php
+++ resources/views/videos/edit.blade.php
@@ -9,5 +9,14 @@
         <a href="{{ url('/videos') }}"><button class="btn btn-primary">Back</button></a><br>
         <hr>
         <h2>Edit Video</h2>
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="form-group">
+                    <label for="uploader">Video Uploader</label>
+                    <input type="text" id="" class="form-control">
+                </div>
+            </div>
+            <div class="col-lg-6"></div>
+        </div>
     </div>
 @endsection
