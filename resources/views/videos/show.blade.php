017a83d861a34674f67a2dfff69f971fe6d90df4
JehanKandy <jehankandy@gmail.com>
2023-07-04 09:44:17 +0530

    Update

--- resources/views/videos/show.blade.php
+++ resources/views/videos/show.blade.php
@@ -4,5 +4,12 @@
     <div class="admin-card">
         <a href="{{ url('/videos') }}"><button class="btn btn-primary">Back</button></a>
         <br><hr><br>
+        <div class="row">
+            <div class="col-lg-6">
+                <h5>Uploader</h5>
+                <h4>{{ $video->uploader }}</h4>
+            </div>
+            <div class="col-lg-6"></div>
+        </div>
     </div>
 @endsection
