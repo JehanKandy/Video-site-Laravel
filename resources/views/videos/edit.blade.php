2feed103bee59c606efe84a57f27e896e66ffecf
JehanKandy <jehankandy@gmail.com>
2023-07-04 10:47:56 +0530

    Update

--- resources/views/videos/edit.blade.php
+++ resources/views/videos/edit.blade.php
@@ -9,14 +9,20 @@
         <a href="{{ url('/videos') }}"><button class="btn btn-primary">Back</button></a><br>
         <hr>
         <h2>Edit Video</h2>
-        <div class="row">
-            <div class="col-lg-6">
-                <div class="form-group">
-                    <label for="uploader">Video Uploader</label>
-                    <input type="text" id="" class="form-control">
+        <form action="{{ url('videos/'. $video->id) }}" method="POST">
+            @csrf
+            @method("PATCH")
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="form-group">
+                        <label for="uploader">Video Uploader</label>
+                        <input type="text" name="add_user" value="{{ $video-> }}" class="form-control" disabled>
+                        <input type="hidden" name="add_user" value="{{ $video->uploder }}">     
+                    </div>
                 </div>
+                <div class="col-lg-6"></div>
             </div>
-            <div class="col-lg-6"></div>
-        </div>
+        </form>
+
     </div>
 @endsection
