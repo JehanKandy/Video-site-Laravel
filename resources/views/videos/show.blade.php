20c2d4cfd3ca15c1ede56d381a3fb7a20cf7d1c7
JehanKandy <jehankandy@gmail.com>
2023-07-04 10:21:10 +0530

    Update

--- resources/views/videos/show.blade.php
+++ resources/views/videos/show.blade.php
@@ -49,5 +49,11 @@
                 @endforeach
             </div>
         </div>
+        <div class="row">
+            <div class="col-lg-6">
+                
+            </div>
+            <div class="col-lg-6"></div>
+        </div>
     </div>
 @endsection
