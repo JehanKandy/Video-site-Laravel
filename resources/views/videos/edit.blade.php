6e153fb981f2249a910e1728f881a63800f4bfd8
JehanKandy <jehankandy@gmail.com>
2023-07-04 10:27:57 +0530

    Update

--- resources/views/videos/edit.blade.php
+++ resources/views/videos/edit.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
     <div class="admin-card">
+        <a href="{{ url('/video') }}"><button class="btn btn-primary">Back</button></a><br>
+        <hr>
         Edit Video
     </div>
 @endsection
